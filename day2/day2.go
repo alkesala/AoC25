c8870b9a21b8abc76e5ee7944c849f9454d86a34
Aleksi Kesälä <nordicdevbox@proton.me>
2025-12-04 09:51:28 +0200

    part2: solved, had stupid printing issues

--- day2/day2.go
+++ day2/day2.go
@@ -10,10 +10,13 @@ import (
 
 func SolveDayTwo() {
 	input := readInput()
-	result1 := calculateTotal(input)
-	fmt.Print(result1)
-}
+	//result1 := calculateTotal(input)
+	result2 := calculateTotal(input)
 
+	fmt.Println("\n=== Day 2 ===")
+	//fmt.Println("Part 1:", result1)
+	fmt.Println("Part 2:", result2)
+}
 func isInvalid(num int) bool {
 	s := strconv.Itoa(num)
 	length := len(s)
@@ -22,6 +25,31 @@ func isInvalid(num int) bool {
 	}
 	mid := length / 2
 	return s[:mid] == s[mid:]
+}
+
+func isInvalid2(num int) bool {
+	s := strconv.Itoa(num)
+	length := len(s)
+	if length <= 1 {
+		return false
+	}
+	for l := 1; l <= length/2; l++ {
+		if length%l == 0 {
+			u := s[:l]
+			isMatch := true
+			for i := l; i < length; i += l {
+				chunk := s[i : i+l]
+				if chunk != u {
+					isMatch = false
+					break
+				}
+			}
+			if isMatch {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func calculateTotal(input string) int {
@@ -33,11 +61,10 @@ func calculateTotal(input string) int {
 		start, _ := strconv.Atoi(wholeId[0])
 		end, _ := strconv.Atoi(wholeId[1])
 		for num := start; num <= end; num++ {
-			if isInvalid(num) {
+			if isInvalid2(num) {
 				total += num
 			}
 		}
-
 	}
 	return total
 }
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/alkesala/AoC25/day1"
 	"github.com/alkesala/AoC25/day2"
+	"github.com/alkesala/AoC25/day3"
 )
 
 func main() {
 	day1.SolveDayOne()
 	day2.SolveDayTwo()
+	day3.SolveDayThree()
 }
